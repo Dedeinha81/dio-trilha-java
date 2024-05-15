f7881ebd58a19dc516598c66820a9fc13deedd9f
deinha81 <168090454+Dedeinha81@users.noreply.github.com>
2024-05-15 20:17:19 -0300

    conta terminal

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
-public class App {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor Digite o numero da agencia");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor Digite o numero da conta");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor Digite seu nome");
+        String nome = scanner.next();
+
+    
+
+        System.out.println("Ola"+nome);
+        System.out.println("Obrigado por criar uma conta em nosso Banco,sua agencia e conta são:" +"agencia" +numero + "conta" +conta );
+        System.out.println("Seu saldo ja esta disponivel para saque");
+
+
+
+
+
+
+    
+
+
+        
+
+
+
+
     }
 }
